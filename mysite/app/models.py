b89a5bf36fee280587a3aa63dccead730235c6e0
Evgeniy <vorobeievgeniy@gmail.com>
2017-01-25 20:23:57 +0300

    Task 3

---
+++ mysite/app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+    address = models.CharField(max_length=400)
+    delivery_date = models.DateTimeField(null=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
---
+++ mysite/app/static/js/validate.js
@@ -0,0 +1,69 @@
+const product_name = document.querySelector('#inputName');
+const product_count = document.querySelector('#inputCount');
+const product_address = document.querySelector('#inputAddress');
+const product_date = document.querySelector('#inputDate');
+
+const submit_button = document.querySelector('#submitButton');
+
+function count_changed() {
+    console.log(this.value);
+    if (product_count.value < 1 || product_count.value > 10) {
+        this.classList.add('invalid');
+        invalid(this);
+    } else {
+        valid(this);
+    }
+}
+
+function address_changed() {
+    console.log(this.value);
+    val = product_address.value.length;
+    if (val < 7 || val > 200) {
+        alert("address incorrect!");
+        invalid(this);
+    } else {
+        valid(this);
+    }
+}
+
+function date_changed() {
+    str = this.value + 'z';
+    date = new Date(str);
+    if (date < Date.now()) {
+        alert("date incorrect!");
+        invalid(this);
+    } else {
+        valid(this);
+    }
+}
+
+function name_changed() {
+    if (this.length < 4) {
+        alert("name short");
+        invalid(this);
+    } else {
+        valid(this);
+    }
+}
+
+function invalid(elem) {
+    elem.classList.add('invalid');
+    submit_button.classList.add('invalidButton');
+    submit_button.disabled = true;
+}
+
+function valid(elem) {
+    elem.classList.remove('invalid');
+    submit_button.classList.remove('invalidButton');
+    if (!product_address.classList.contains('invalid') &&
+        !product_date.classList.contains('invalid') &&
+        !product_count.classList.contains('invalid')) {
+        submit_button.disabled = false;
+    }
+}
+
+
+product_name.onchange = name_changed;
+product_count.onchange = count_changed;
+product_address.onchange = address_changed;
+product_date.onchange = date_changed;
